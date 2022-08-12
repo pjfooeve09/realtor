cecedda8dccb2d2dbadf2ba86b69687f8c87dec2
Parham Javadi <parham@Parhams-MacBook-Pro.local>
2022-08-12 12:56:10 -0700

    first commit

---
+++ utils/FetchApi.js
@@ -0,0 +1,8 @@
+export const baseUrl = "https://bayut.p.rapidapi.com"
+
+export const config = {
+      headers: {
+        'X-RapidAPI-Key': 'f304a53aa0msh94f4b87b20b7047p130d3bjsn453105195bfa',
+        'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+      }
+}
